6c555eb172f2205493abf40cfb81d667bbcedce1
Josh Benner <josh@bennerweb.com>
2023-05-17 14:11:01 -0400

    Remove commented code

--- components/daikin_s21/s21.cpp
+++ components/daikin_s21/s21.cpp
@@ -1,7 +1,3 @@
-// #include "esphome/core/defines.h"
-// #include "esphome/core/component.h"
-// #include "esphome/core/hal.h"
-// #include "esphome/core/log.h"
 #include "s21.h"
 
 using namespace esphome;
