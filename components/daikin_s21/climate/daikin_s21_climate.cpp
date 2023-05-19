71b210be27e1ca6f5e7f202f390cc89f8275bfac
Josh Benner <josh@bennerweb.com>
2023-05-19 16:36:09 -0400

    Fix reporting power off action

--- components/daikin_s21/climate/daikin_s21_climate.cpp
+++ components/daikin_s21/climate/daikin_s21_climate.cpp
@@ -174,13 +174,14 @@ void DaikinS21Climate::update() {
   if (this->s21->is_ready()) {
     if (this->s21->is_power_on()) {
       this->mode = this->d2e_climate_mode(this->s21->get_climate_mode());
+      this->action = this->d2e_climate_action();
     } else {
       this->mode = climate::CLIMATE_MODE_OFF;
+      this->action = climate::CLIMATE_ACTION_OFF;
     }
     this->set_custom_fan_mode_(this->d2e_fan_mode(this->s21->get_fan_mode()));
     this->swing_mode = this->d2e_swing_mode(this->s21->get_swing_v(),
                                             this->s21->get_swing_h());
-    this->action = this->d2e_climate_action();
     this->current_temperature = this->s21->get_temp_inside();
     this->target_temperature = this->s21->get_setpoint();
     this->publish_state();
