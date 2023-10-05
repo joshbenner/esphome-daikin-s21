46535d006a93d6f03d1ae4906840df1d9b9ee402
unknown <C:\Users\Dmitry\AppData\Roaming\The Bat!>
2023-10-04 22:37:38 -0400

    Change Disabled mode to Auto

--- components/daikin_s21/s21.h
+++ components/daikin_s21/s21.h
@@ -7,8 +7,8 @@ namespace esphome {
 namespace daikin_s21 {
 
 enum class DaikinClimateMode : uint8_t {
-  Disabled = '0',
-  Auto = '1',
+  Disabled = '1',
+  Auto = '0',
   Dry = '2',
   Cool = '3',
   Heat = '4',
